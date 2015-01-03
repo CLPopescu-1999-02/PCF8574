25156c66b702b382f11dcc6180861f2f1641d040
Peter Majchrak <petoknm@gmail.com>
2015-01-03 08:37:08 +0100

    Added documentation

---
+++ documentation/html/search/all_1.js
@@ -0,0 +1,17 @@
+var searchData=
+[
+  ['pcf8574_2ec',['pcf8574.c',['../pcf8574_8c.html',1,'']]],
+  ['pcf8574_2eh',['pcf8574.h',['../pcf8574_8h.html',1,'']]],
+  ['pcf8574_5fdeinit',['PCF8574_DeInit',['../pcf8574_8c.html#a06a2a44d5c5b7a59100e4b8c92789aea',1,'PCF8574_DeInit(PCF8574_HandleTypeDef *handle):&#160;pcf8574.c'],['../pcf8574_8h.html#a06a2a44d5c5b7a59100e4b8c92789aea',1,'PCF8574_DeInit(PCF8574_HandleTypeDef *handle):&#160;pcf8574.c']]],
+  ['pcf8574_5ferror',['PCF8574_ERROR',['../pcf8574_8h.html#acb986b4815c5ba43b8c1800053f3ed25a1d02f271b5126d02f29ba35e67fd387d',1,'pcf8574.h']]],
+  ['pcf8574_5fhandletypedef',['PCF8574_HandleTypeDef',['../struct_p_c_f8574___handle_type_def.html',1,'']]],
+  ['pcf8574_5fi2c_5faddress_5fmask',['PCF8574_I2C_ADDRESS_MASK',['../pcf8574_8h.html#adec3cc60dd2b676f53c7c2883b528ef0',1,'pcf8574.h']]],
+  ['pcf8574_5finit',['PCF8574_Init',['../pcf8574_8c.html#a6f858372bee0a5a1690440809570befd',1,'PCF8574_Init(PCF8574_HandleTypeDef *handle):&#160;pcf8574.c'],['../pcf8574_8h.html#a6f858372bee0a5a1690440809570befd',1,'PCF8574_Init(PCF8574_HandleTypeDef *handle):&#160;pcf8574.c']]],
+  ['pcf8574_5fok',['PCF8574_OK',['../pcf8574_8h.html#acb986b4815c5ba43b8c1800053f3ed25ab3e80ac05337737d6e8feaab92df2526',1,'pcf8574.h']]],
+  ['pcf8574_5fread',['PCF8574_Read',['../pcf8574_8c.html#aefd68f9b6e4fd39dec4bf3f83f04f5d1',1,'PCF8574_Read(PCF8574_HandleTypeDef *handle, uint8_t *val):&#160;pcf8574.c'],['../pcf8574_8h.html#aefd68f9b6e4fd39dec4bf3f83f04f5d1',1,'PCF8574_Read(PCF8574_HandleTypeDef *handle, uint8_t *val):&#160;pcf8574.c']]],
+  ['pcf8574_5fresult',['PCF8574_RESULT',['../pcf8574_8h.html#acb986b4815c5ba43b8c1800053f3ed25',1,'pcf8574.h']]],
+  ['pcf8574_5ftype0pins',['PCF8574_Type0Pins',['../pcf8574_8h.html#ab675a084f7abed2eadda6cfd29795e0b',1,'pcf8574.h']]],
+  ['pcf8574_5fwrite',['PCF8574_Write',['../pcf8574_8c.html#ad654f02d253725144dea2bd67ec769fe',1,'PCF8574_Write(PCF8574_HandleTypeDef *handle, uint8_t val):&#160;pcf8574.c'],['../pcf8574_8h.html#ad654f02d253725144dea2bd67ec769fe',1,'PCF8574_Write(PCF8574_HandleTypeDef *handle, uint8_t val):&#160;pcf8574.c']]],
+  ['pcf_5fi2c_5faddress',['PCF_I2C_ADDRESS',['../struct_p_c_f8574___handle_type_def.html#a6dffbcc6620a5fc10e355941f764b5e0',1,'PCF8574_HandleTypeDef']]],
+  ['pcf_5fi2c_5ftimeout',['PCF_I2C_TIMEOUT',['../struct_p_c_f8574___handle_type_def.html#aab54a3fd634f0730d4181d8a079a6755',1,'PCF8574_HandleTypeDef']]]
+];
